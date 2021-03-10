0733f994998c2f196f7eeb9ca7e775276c40b51b
Airashtest <80360833+Airashtest@users.noreply.github.com>
2021-03-10 10:32:51 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -44,10 +44,10 @@ def stats(update, context):
 @run_async
 def start(update, context):
     start_string = f'''
-Hi, I'm Slam, a multipurpose bot for [hafitz](t.me/hafitzXD)
+Hi, I'm pryo, mega bot at currently working @VegaCloud
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
-    update.effective_message.reply_photo("https://telegra.ph/file/db03910496f06094f1f7a.jpg", start_string, parse_mode=ParseMode.MARKDOWN)
+    update.effective_message.reply_photo("https://telegra.ph/file/28ef630fde21614248d96.jpg", start_string, parse_mode=ParseMode.MARKDOWN)
 
 
 @run_async
