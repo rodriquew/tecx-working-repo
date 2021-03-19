66c861a76e292f7093c2bc72d8e330f37798bb3a
Airashtest <80360833+Airashtest@users.noreply.github.com>
2021-03-19 23:33:15 +0530

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -4,9 +4,9 @@ export MAX_CONCURRENT_DOWNLOADS=7
 
 aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 --check-certificate=false\
    --max-connection-per-server=10 --rpc-max-request-size=1024M \
-   --bt-tracker="[$tracker_list]" --bt-max-peers=0 --seed-time=0.01 --min-split-size=10M \
+   --bt-tracker="[$tracker_list]" --bt-max-peers=0 --seed-time=0.01 --min-split-size=10M --bt-stop-timeout=1200 \
    --follow-torrent=mem --split=10 \
-   --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
+   --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED  \
    --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS \
    --peer-id-prefix=-qB4220- --user-agent=qBittorrent/4.2.2 \
    --disk-cache=64M --file-allocation=prealloc --continue=true \
